5a945e174f313f9092e09292f76dbb89e7200431
FengGuanwei <853026205@qq.com>
2022-01-22 19:51:32 +0800

    "19:51提交"

--- Model01/src/com/wing/Person.java
+++ Model01/src/com/wing/Person.java
@@ -3,4 +3,29 @@ package com.wing;
 public class Person {
     private int age;
     private String name;
+    private double height;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
 }
